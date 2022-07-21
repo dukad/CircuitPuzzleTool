9fa1c069d603065c5bda695317d718c3b06d8cd0
dukad <du.kad@northeastern.edu>
2022-07-21 10:53:49 -0400

    Pre-click code

--- app.js
+++ app.js
@@ -1,44 +1,18 @@
- import Cell from './src/cell.js';
+import Cell from './src/cell.js';
 
 // create a new pixi application, all things inside the application must fit within this window
 const app = new PIXI.Application(
     {
-        width: window.outerWidth,
-        height: window.outerHeight,
+        width: window.innerWidth,
+        height: window.innerHeight,
         backgroundColor: 0x11111,
-        resolution: window.devicePixelRatio || 1,
+        // resolution: window.devicePixelRatio || 1,
     }
 )
 
 // tell the html document to display the pixi application
 document.body.appendChild(app.view)
 
-// Cell class meant to be in cell.js after import problems fixed
- /*
-class Cell {
-    constructor(x_coordinate, y_coordinate, dimension, rect, color) {
-        this.x = x_coordinate;
-        this.y = y_coordinate;
-        this.dimension = dimension;
-        this.rect = rect;
-        this.color = color;
-
-    }
-
-    draw(app, graphics) {
-        graphics.lineStyle(2, 0x1f1f1f, 1)
-        graphics.beginFill(this.color)
-        graphics.drawRect(this.x, this.y, this.dimension, this.dimension)
-        graphics.endFill()
-        app.stage.addChild(graphics)
-    }
-
-    onClick(app, graphics) {
-        this.color = 0x04b504
-        return this.draw(app, graphics)
-}
-}
-*/
 // create container for all cells
 let graphic = new PIXI.Graphics();
 app.stage.addChild(graphic);
@@ -55,6 +29,7 @@ let cell = [];
 for (let i = 0; i < grid_height; i++) {
     cell[i] = []
 }
+
 //create a matrix of cells
 for (let i = 0; i < grid_height; i++) {
     for (let j = 0; j < grid_width; j++)
@@ -63,15 +38,17 @@ for (let i = 0; i < grid_height; i++) {
         rect = new PIXI.Rectangle(j * 45, i * 45, cell_dimension, cell_dimension);
         let newCell = new Cell(j * 45, i * 45, cell_dimension, rect, 0x000000)
         newCell.draw(app, graphic)
+        // newCell.onClick(app, graphic)
         cell[i][j] = newCell;
         cell[i][j].rect.interactive = true;
         cell[i][j].rect.buttonMode = true;
     }
 }
 
+
 for (let i = 0; i < grid_height; i++) {
     for (let j = 0; j < grid_width; j++) {
-        cell[i][j].rect.onclick = function() {console.log('clicked')}
+        // cell[i][j].rect.on("pointerdown",
     }
 }
 
