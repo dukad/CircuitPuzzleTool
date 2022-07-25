044e603aefad9cc81dd5a50263a2a32cea7588b5
dukad <du.kad@northeastern.edu>
2022-07-25 15:33:24 -0400

    Organization

--- src/wire.js
+++ src/wire.js
@@ -6,34 +6,12 @@ export default class Wire extends Cell {
         this.display_directions = new Set();
     }
 
-    find_direction(part) {
-        let x_diff = part.x - this.x; //find the direction which the cell is in
-        let y_diff = part.y - this.y;
-        let direction = 0; // initialize as top-left
-        direction += (y_diff + 1)*3;
-        direction += (x_diff + 1);
-        if (direction !== 4) { //if the direction is not in the center
-            return direction;
-        } else {
-            return null;
-        }
-    }
-
-    reverse_direction(dir) { //reverse the direction
-        let rem = dir % 3;
-        let div = (dir - rem) / 3;
-        let rem_reverse = Math.abs(rem - 2);
-        let div_reverse = Math.abs(div - 2);
-        div_reverse *= 3;
-        return (div_reverse + rem_reverse);
-    }
-
-    connect(part) { //connect two adjacent wires
-        this.connected_parts.add(part);
-        part.connected_parts.add(this);
-        let direction = this.find_direction(part);
+    display_orient(direction) {
         this.display_directions.add(direction)
-        part.display_directions.add(this.reverse_direction(direction))
+    }
+
+    undisplay(direction) {
+        this.display_directions.delete(direction)
     }
 
     create_node(size) { //draw a circle of varying size
@@ -52,7 +30,7 @@ export default class Wire extends Cell {
         } else {
             this.create_node(this.dimension / 9) //create a small node
         }
-        this.graphic.lineStyle(10, 0x04b504); // change the linestyle to thick green
+        this.graphic.lineStyle(5, 0x04b504); // change the linestyle to thick green
         this.display_directions.forEach(i => {
             let dir = i;
             let x_change = dir % 3; // 0, 1, 2
@@ -66,5 +44,7 @@ export default class Wire extends Cell {
             this.app.stage.addChild(this.graphic);
         });
         // console.log(this.x, this.y, this.display_directions, this.connected_parts.size)
+        console.log(this.connected_parts);
     }
 }
+
